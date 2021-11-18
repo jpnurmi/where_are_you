1f0325cbb0caa499c4ec314be1e3300dc40ae399
J-P Nurmi <jp.nurmi@canonical.com>
2021-11-18 14:07:17 +0100

    Get rid of generated classes

--- lib/geo_service.dart
+++ lib/geo_service.dart
@@ -1,26 +1,87 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:meta/meta.dart';
 import 'package:geocoder_offline/geocoder_offline.dart';
-
-part 'geo_service.freezed.dart';
-part 'geo_service.g.dart';
 
 const _kBaseUrl = 'http://geoname-lookup.ubuntu.com/';
 
-@freezed
-class GeoLocation with _$GeoLocation {
-  factory GeoLocation({
+class GeoLocation {
+  GeoLocation({
+    this.name,
+    this.admin1,
+    this.country,
+    this.longitude,
+    this.latitude,
+  });
+
+  final String? name;
+  final String? admin1;
+  final String? country;
+  final double? longitude;
+  final double? latitude;
+
+  GeoLocation copyWith({
     String? name,
     String? admin1,
     String? country,
-    @JsonKey(fromJson: double.tryParse) double? longitude,
-    @JsonKey(fromJson: double.tryParse) double? latitude,
-  }) = _GeoLocation;
+    double? longitude,
+    double? latitude,
+  }) {
+    return GeoLocation(
+      name: name ?? this.name,
+      admin1: admin1 ?? this.admin1,
+      country: country ?? this.country,
+      longitude: longitude ?? this.longitude,
+      latitude: latitude ?? this.latitude,
+    );
+  }
 
-  factory GeoLocation.fromJson(Map<String, dynamic> json) =>
-      _$GeoLocationFromJson(json);
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'admin1': admin1,
+      'country': country,
+      'longitude': longitude,
+      'latitude': latitude,
+    };
+  }
+
+  factory GeoLocation.fromJson(Map<String, dynamic> map) {
+    return GeoLocation(
+      name: map['name'],
+      admin1: map['admin1'],
+      country: map['country'],
+      longitude: double.tryParse(map['longitude']),
+      latitude: double.tryParse(map['latitude']),
+    );
+  }
+
+  @override
+  String toString() =>
+      'GeoLocation(name: $name, admin1: $admin1, country: $country, longitude: $longitude, latitude: $latitude)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is GeoLocation &&
+        other.name == name &&
+        other.admin1 == admin1 &&
+        other.country == country &&
+        other.longitude == longitude &&
+        other.latitude == latitude;
+  }
+
+  @override
+  int get hashCode {
+    return Object.hash(
+      name.hashCode,
+      admin1.hashCode,
+      country.hashCode,
+      longitude.hashCode,
+      latitude.hashCode,
+    );
+  }
 }
 
 class GeoService {
